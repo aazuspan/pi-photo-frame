3d6fe1a7b0eead532952643259d0964857dfd80d
Aaron Zuspan <aa.zuspan@gmail.com>
2023-10-24 18:28:26 -0700

    Use threading to handle sleep mode

--- src/frame/motion_sensor.py
+++ src/frame/motion_sensor.py
@@ -1,54 +1,67 @@
+import threading
 import os
 import time
 
 
 class MotionSensor:
+    """A motion sensor that controls sleep state.
+    
+    On initialization, a thread is started that watches the motion sensor and
+    sets the sleep state accordingly. When sleeping, the main `update` function
+    will block until motion is detected.
+    """
     def __init__(self, motion_gpio, sleep_after=1_200, motion_threshold=50_000):
         try:
             from gpiozero import MotionSensor
         except ImportError:
             raise ImportError("gpiozero is required for motion detection.")
         
-        self.is_awake = True
         self.sensor = MotionSensor(motion_gpio, queue_len=30)
         self.sleep_after = sleep_after
         self.motion_threshold = motion_threshold
-        self.last_motion = time.time()
+
+        self.is_asleep = threading.Event()
+        threading.Thread(target=self.check_loop).start()
 
     def update(self):
-        """Check for motion and sleep after a period of inactivity."""
-        self.motion_detected()
-
-        if self.time_to_sleep():
-            self.sleep()
-
-    def time_to_sleep(self):
-        return time.time() - self.last_motion > self.sleep_after
+        """Block execution if asleep."""
+        self.is_asleep.wait()
 
     def motion_detected(self):
-        """Check if motion over a threshold is detected."""
+        """Check the motion sensor for motion above a threshold."""
+        motion_confirmed = False
         motion_count = 0
-        while self.sensor.motion_detected:
+        while self.sensor.motion_detected and not motion_confirmed:
             motion_count += 1
-            # Interrupt the check loop as soon as the threshold is met
             if motion_count > self.motion_threshold:
-                self.last_motion = time.time()
-                return True
-            
-        return False
+                motion_confirmed = True
+        
+        return motion_confirmed
+
+    def check_loop(self):
+        """Watch the motion sensor and set the sleep state."""
+        last_motion = time.time()
+
+        while True:
+            motion_confirmed = self.motion_detected()
+                    
+            if motion_confirmed:
+                last_motion = time.time()
+            time_to_sleep = time.time() - last_motion > self.sleep_after
+
+            if self.is_asleep.is_set() and motion_confirmed:
+                self.wake()
+            elif not self.is_asleep.is_set() and time_to_sleep:
+                self.sleep()
     
     def sleep(self):
-        """Put the display to sleep and wait for motion or IR."""
-        self.is_awake = False
+        """Put the display to sleep and wait for motion."""
+        self.is_asleep.set()
         hdmi_off()
 
-        while not self.is_awake:
-            if self.motion_detected() or self.irw.get_key():
-                self.wake()
-    
     def wake(self):
         """Wake the display from sleep."""
-        self.is_awake = True
+        self.is_asleep.clear()
         hdmi_on()
 
 
