20686c45a5904934702a1587e3e5656df6110f05
Aaron Zuspan <50475791+aazuspan@users.noreply.github.com>
2023-10-26 00:36:12 -0700

    IRW refactor (#4)
    * Refactor IRW and make optional

--- src/frame/irw.py
+++ src/frame/irw.py
@@ -1,59 +1,26 @@
-#!/usr/bin/env python
-
-'''
-Read lirc output, in order to sense key presses on an IR remote.
-
-In order for commands to be read, a configuration file for your remote must exist in
-your lirc directory at /etc/lirc/lircd.conf.d. Common remote configuration files can be
-found and downloaded from http://lirc-remotes.sourceforge.net/remotes-table.html.
-
-Based on pyirw.py by Akkana Peck, https://github.com/akkana/scripts/blob/master/rpi/pyirw.py
-'''
-
 import socket
 
 
 class IRW:
-    def __init__(self, socket_path="/var/run/lirc/lircd", timeout=0.01, blocking=0):
-        self._socket_path = socket_path
-        # How long to look for data each call (longer values will slow down the rest of the program, but are less likely to miss commands)
-        self._timeout = timeout
-        # Should the socket block the rest of the program until data is received? Timeout must be None to enable blocking
-        self._blocking = blocking
-        # Automatically connect to a socket
-        self._sock = self._connect()
+    """
+    Read lirc output, in order to sense key presses on an IR remote.
 
-        
-    # Initialize and connect to the socket for reading IR commands
-    def _connect(self):
-        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        sock.setblocking(self._blocking)
-        sock.settimeout(self._timeout)
-        sock.connect(self._socket_path)
-        
-        return sock
-        
-    # Check if an IR key is received and return either the command or None
+    In order for commands to be read, a configuration file for your remote must exist in
+    your lirc directory at /etc/lirc/lircd.conf.d. Common remote configuration files can be
+    found and downloaded from http://lirc-remotes.sourceforge.net/remotes-table.html.
+
+    Based on pyirw.py by Akkana Peck, https://github.com/akkana/scripts/blob/master/rpi/pyirw.py
+    """
+    def __init__(self, socket_path="/var/run/lirc/lircd", timeout=0.01, blocking=False):
+        self._sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self._sock.setblocking(blocking)
+        self._sock.settimeout(timeout)
+        self._sock.connect(socket_path)
+
     def get_key(self):
+        """Return a command if one is received."""
         try:
-            # Check if IR data is available on the socket
-            data = self._sock.recv(128)
-            data = data.strip()
-            # Decode the key command from the received data
-            try:
-                command = data.decode().split()[2]
-                return command
-            # If the data is somehow corrupted and can't be indexed
-            except IndexError:
-                return None
-        # If no data is received
+            data = self._sock.recv(128).strip()
+            return data.decode().split()[2]
         except Exception:
             return None
-
-if __name__ == '__main__':
-    irw = IRW()
-
-    while True:
-        command = irw.get_key()
-        if command:
-            print(command)
