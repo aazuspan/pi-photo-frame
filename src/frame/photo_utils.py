e1043960a60ff769bdf962a8eb72454180ab622c
Aaron Zuspan <50475791+aazuspan@users.noreply.github.com>
2023-10-27 18:07:07 -0700

    Refactor (#6)
    * Refactor and skip photo rotation unless needed
    
    * Fix skipped rotation
    
    * Fix exif rotation bug
    
    * Cleanup and refactoring
    
    * Instantiate display before camera
    
    * Add missing param
    
    * Revert text space
    
    * Revert text spacing

---
+++ src/frame/photo_utils.py
@@ -0,0 +1,25 @@
+from PIL import Image
+from pi3d import Texture
+from typing import Union
+
+
+def load_photo_texture(filepath):
+    """Load a photo from a file and return a pi3d.Texture."""
+    image = Image.open(filepath)
+    image = fix_exif_rotation(image)
+    return Texture(image, blend=True, m_repeat=True)
+
+
+def fix_exif_rotation(image):
+    """Rotate an image based on its EXIF orientation tag."""
+    EXIF_ORIENTATION_TAG = 274
+    EXIF_ORIENTATION_DICT = {3: 180, 4: 180, 5: 270, 6: 270, 7: 90, 8: 90}
+    
+    exif_data = image.getexif()
+    exif_orientation: Union[int, None] = exif_data.get(EXIF_ORIENTATION_TAG, None)
+    rotate_angle: int = EXIF_ORIENTATION_DICT.get(exif_orientation, 0)
+
+    if rotate_angle:
+        image = image.rotate(rotate_angle, expand=True)
+
+    return image
